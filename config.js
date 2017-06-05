a6e9a04298bcc42642c8415ac834b3fb353ad0f7
Paula Q <pnquach@ucsd.edu>
2017-06-05 14:27:13 -0700

    minor changes

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const WIT_TOKEN = process.env.WIT_TOKEN
+const WIT_TOKEN = process.env.WIT_TOKEN || '532SMYDNILXEVS7CRD37ZLP2D6KLAGI4';
 if (!WIT_TOKEN) {
   throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.')
 }
 
+//EAATPOypj3vwBAIfuW9qOxWiEgUL1ebSij52C3zTZCcnwwcSiKJ4D5YuHgz35G6uDrZBtOEtZCMFWQpyjScWMQrFK1p16EcEny83i7LZAY1W7d6YHuPvCIchqY44USXH39mgSvzZBgEFswVKRNaMcOJFJ0g3zi3ArEi18GjlYIaQZDZD
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAATPOypj3vwBAIfuW9qOxWiEgUL1ebSij52C3zTZCcnwwcSiKJ4D5YuHgz35G6uDrZBtOEtZCMFWQpyjScWMQrFK1p16EcEny83i7LZAY1W7d6YHuPvCIchqY44USXH39mgSvzZBgEFswVKRNaMcOJFJ0g3zi3ArEi18GjlYIaQZDZD';
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAPCnjgJAvkBACZBidHZBZACz35igIGqULElTntgiAgVzIxVxAX7Y38Eb1N2FD2bKGZBT8OlL6a6F0qEJsWhYdWeZCdsbaZCTd13HT8QEwNPRa3owTNuAeqLG5HZAAzkOC1jiGhR90Nlb8mk3cylI0FsuZBf6lWWzMB5ZA3cXXVzgCQZDZD';
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
